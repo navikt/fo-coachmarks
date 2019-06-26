74ea8ae5f4f15aa43fcef341d4c928608934fd7f
Stian Horn Stensli <stian.hs@hotmail.com>
2019-06-26 12:44:41 +0200

    modified package.json so that exports works from inport

---
+++ coachmarks-lib/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Test() {
+    return <h1>Hello World</h1>;
+  }
+  
+  export default Test;
--- coachmarks-lib/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TextInput from "./lib/example";
-
-ReactDOM.render(<TextInput  label={""} onChange={(e) => {return e}} type={""} value={"Skriv noe her..."}/>, document.getElementById("root"));
